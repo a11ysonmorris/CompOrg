98bd17fbcd30d73117f597d8ff6beb08605e3185
a11ysonmorris <144363130+a11ysonmorris@users.noreply.github.com>
2025-09-30 17:11:38 -0400

    Update lab1p2q1.c

--- lab1/lab1p2q1.c
+++ lab1/lab1p2q1.c
@@ -1,3 +1,20 @@
+/*
+ * Programmer: Allyson Morris
+ * 
+ * Lab 1 - Part 2, Question 1
+ *
+ * Problem: This program highlights basic functions in C. 
+ * It prints a message, calculates the difference between two integers, 
+ * and performs a division of two floating-point numbers.
+ *
+ * Solution Method: 
+ * - The program has three functions: 
+ *   1) print_half_message: prints a static message.
+ *   2) print_difference: takes two integers, calculates their difference, and prints it.
+ *   3) print_division: takes two floats, divides them, and prints the result.
+ * - The main function calls these three functions with example values.
+ */
+
 #include <stdio.h>
 
 // Function a
